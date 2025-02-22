3cdbcd10b1e90bb3e11a475d1c28802a3f2c5d9c
mothnklevin <klevin2775@gmail.com>
2025-02-22 19:30:01 +0000

    2.22 LAB4 ex

---
+++ LAB-YZ/com6012/mywork/lab4/LAB4_ex.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=LAB4_EX
+
+#SBATCH --time=00:30:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=5G
+#SBATCH --output=/users/acw24yz/com6012/mywork/Output/Lab4.txt  # task output log
+#SBATCH --mail-user=yzhang851@sheffield.ac.uk
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+module load Java/17.0.4
+module load Anaconda3/2022.05
+
+source activate myspark
+
+#spark-submit --driver-memory 5g --executor-memory 5g --master local[2] ./test.py
+#spark-submit ./LAB3_01_BaseMD.py
+spark-submit --driver-memory 4g --executor-memory 5g --master local[2] ./LAB4_01.py
+
+
+
+grep -v "INFO"  /users/acw24yz/com6012/mywork/Output/Lab4.txt > \
+                /users/acw24yz/com6012/mywork/Output/Lab4_clean.txt
+
+echo "LAB4  completed successfully"
